f5342f8ceaacba6e3debc69c5132eb75717ce123
SamHarron <harronsam@gmail.com>
2020-12-06 18:45:52 -0500

    commit tree f19251aba23400d382fcd2fda34c81d4b08227d7

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,30 @@
 class Waiter
+        attr_accessor :name, :years
 
+        @@all = []
+
+        def initialize(name, years)
+            @name = name
+            @years = years
+            @@all << self
+        end
+
+        def self.all
+            @@all
+        end
+
+        def new_meal(customer, total, tip=0)
+            Meal.new(self, customer, total, tip)
+        end
+
+        def meals
+            Meal.all.select {|meal| meal.waiter == self}
+        end
+
+        def best_tipper
+            largest_tip = meals.max do |x, y| 
+                x.tip <=> y.tip
+        end
+            largest_tip.customer
+        end
 end
